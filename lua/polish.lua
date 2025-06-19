1c15a9813fe5281642431d9fdd41a67cc1a0fe17
Vincent Delbar <vincent.delbar@latelescop.fr>
2025-06-19 14:28:45 +0200

    Moving to astronvim 5

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,29 +1,25 @@
--- This will run last in the setup process and is a good place to configure
--- things like custom filetypes. This just pure lua so anything that doesn't
+-- This will run last in the setup process.
+-- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
-
---[[ Set up custom filetypes
-vim.filetype.add {
-  extension = {
-    foo = "fooscript",
-  },
-  filename = {
-    ["Foofile"] = "fooscript",
-  },
-  pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
-  },
-}
-]]
 
 require'lspconfig'.pylsp.setup{
   settings = {
     pylsp = {
       plugins = {
-        pycodestyle = {
-          ignore = {},
-          maxLineLength = 100
-        }
+        -- formatter options
+        black = { enabled = true },
+        autopep8 = { enabled = false },
+        yapf = { enabled = false },
+        -- linter options
+        pylint = { enabled = true, executable = "pylint", args = {"--max-line-length=100"} },
+        pyflakes = { enabled = false },
+        pycodestyle = { enabled = false },
+        -- type checker
+        pylsp_mypy = { enabled = false },
+        -- auto-completion options
+        jedi_completion = { fuzzy = true },
+        -- import sorting
+        pyls_isort = { enabled = true },
       }
     }
   }
